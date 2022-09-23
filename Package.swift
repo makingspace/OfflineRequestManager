0afd37f04e72ab26736785f7f3e4da5da4b2c8ec
DanBurkhardt <dan@gigabitelabs.com>
2022-09-22 22:25:51 -0400

    cleanup default comments on Package.swift

--- Package.swift
+++ Package.swift
@@ -9,7 +9,6 @@ let package = Package(
     	.iOS(.v13),
     ],
     products: [
-        // Products define the executables and libraries a package produces, and make them visible to other packages.
         .library(
             name: "OfflineRequestManager",
             targets: ["OfflineRequestManager"]),
@@ -18,8 +17,6 @@ let package = Package(
         .package(url: "https://github.com/Alamofire/Alamofire.git", .upToNextMajor(from: "5.6.1"))
     ],
     targets: [
-        // Targets are the basic building blocks of a package. A target can define a module or a test suite.
-        // Targets can depend on other targets in this package, and on products in packages this package depends on.
         .target(
             name: "OfflineRequestManager",
             dependencies: [
